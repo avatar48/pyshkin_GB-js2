1f9fcf69d71c9fd8886ab9d61c07b260b8ce7d30
Avatar <avatar48@yandex.ru>
2019-08-13 15:11:03 +0300

    Пункты 1, 2 и 3

--- js/main.js
+++ js/main.js
@@ -1,20 +1,28 @@
+class Cart {
+
+}
+
+class CartItem {
+
+}
+
 const goods = [
-    { title: 'Shirt', price: 150 },
-    { title: 'Socks', price: 50 },
-    { title: 'Jacket', price: 350 },
-    { title: 'Shoes', price: 250 },
-    { price: 250 },
-    { title: 'Coat'},
-  ];
+  { title: 'Shirt', price: 150 },
+  { title: 'Socks', price: 50 },
+  { title: 'Jacket', price: 350 },
+  { title: 'Shoes', price: 250 },
+  { price: 250 },
+  { title: 'Coat'},
+];
 
 
-  const renderGoodsItem = (title = "unknown", price = "unknown", id) => {
-    return `<div class="goods-item" data-id="${id}"><h3>${title}</h3><p>${price}</p></div>`;
-  };
-  
-  const renderGoodsList = (list) => {
-    let goodsList = list.map((item, index) => renderGoodsItem(item.title, item.price, index));
-    document.querySelector('.goods-list').innerHTML = goodsList.join('');
-  }
-  
-  renderGoodsList(goods);
+const renderGoodsItem = (title = "unknown", price = "unknown", id) => {
+  return `<div class="goods-item" data-id="${id}"><h3>${title}</h3><p>${price}</p></div>`;
+};
+
+const renderGoodsList = (list) => {
+  let goodsList = list.map((item, index) => renderGoodsItem(item.title, item.price, index));
+  document.querySelector('.goods-list').innerHTML = goodsList.join('');
+}
+
+renderGoodsList(goods);
